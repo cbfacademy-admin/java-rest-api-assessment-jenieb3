87dcf7fcb3fd992074495280a5c24ddf2ed5bef8
jenieb3 <117836200+jenieb3@users.noreply.github.com>
2023-10-25 18:14:10 +0100

    refactor the App.java

--- src/main/java/com/cbfacademy/apiassessment/App.java
+++ src/main/java/com/cbfacademy/apiassessment/App.java
@@ -7,16 +7,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class App {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s", name);
-	}
-
 }
+
+
